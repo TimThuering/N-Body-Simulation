72490b96b236224bc03e7a41031c0e7c0f811c41
TimThuering <56958925+TimThuering@users.noreply.github.com>
2022-10-25 12:31:40 +0200

    included cxxopts for parsing program arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include "Body.h"
 #include "InputParser.h"
-#include <iostream>
-int main() {
-std::string path = "";
-InputParser::parse_input(path);
+#include <cxxopts.hpp>
+
+int main(int argc, char *argv[]) {
+    cxxopts::Options arguments("N-Body-Simulation");
+
+    // add "path" as program argument
+    arguments.add_options()
+            ("path", "Path to a .csv file containing the data for the simulation", cxxopts::value<std::string>());
+
+    auto result = arguments.parse(argc, argv);
+    std::string path = result["path"].as<std::string>();
+
+    // parse the csv file containing the simulation data
+    InputParser::parse_input(path);
 }
