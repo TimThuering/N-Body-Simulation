edc01103c719f3481ae20f07f6e831d0cc6ef1ff
TimThuering <56958925+TimThuering@users.noreply.github.com>
2023-02-10 11:24:15 +0100

    update configuration

--- src/utility/Configuration.cpp
+++ src/utility/Configuration.cpp
@@ -1,5 +1,6 @@
 #include "Configuration.hpp"
 #include <cmath>
+#include <sycl/sycl.hpp>
 
 // initialize with default values
 std::size_t configuration::numberOfBodies = 0;
@@ -7,13 +8,18 @@ double configuration::epsilon2 = std::pow(10, -22);
 
 int configuration::naive_algorithm::tileSizeNaiveAlg = 64;
 
+
+
 std::size_t configuration::barnes_hut_algorithm::storageSizeParameter = 0;
 int configuration::barnes_hut_algorithm::AABBWorkItemCount = 100;
 int configuration::barnes_hut_algorithm::octreeWorkItemCount = 640;
 double configuration::barnes_hut_algorithm::theta = 1.05;
+int configuration::barnes_hut_algorithm::maxBuildLevel = 7;
+std::size_t configuration::barnes_hut_algorithm::stackSize = 0;
 
 void configuration::loadConfiguration(std::size_t bodyCount) {
     configuration::numberOfBodies = bodyCount;
-
     configuration::barnes_hut_algorithm::storageSizeParameter = 16 * numberOfBodies;
+    configuration::barnes_hut_algorithm::stackSize = 16 * (std::size_t) std::ceil(std::log2(bodyCount));
+
 }
--- src/utility/Configuration.hpp
+++ src/utility/Configuration.hpp
@@ -2,6 +2,8 @@
 #define N_BODY_SIMULATION_CONFIGURATION_HPP
 
 #include <cstddef>
+#include <string>
+#include <sycl/sycl.hpp>
 
 namespace configuration {
 
@@ -33,6 +35,13 @@ namespace configuration {
         extern double theta;
 
         /*
+         * Parameter that determines the size of the stack used to traverse the tree during the acceleration computation
+         * of the Barnes-Hut algorithm.
+         * Depends on the log_2 of the number of bodies in the example.
+         */
+        extern std::size_t stackSize;
+
+        /*
          * Parameter used to determine the amount of storage that will be allocated for various fields needed by the
          * Barnes-Hut algorithm implementation.
          * The value will depend on the total number of bodies used for the simulation in order to scale with it.
@@ -44,6 +53,12 @@ namespace configuration {
 
         // Number of work-items used for the octree creation
         extern int octreeWorkItemCount;
+
+        /*
+         * If ParallelOctreeTopDownSubtree is used, this parameter determines the maximum level to which the octree is
+         * build in the first Phase.
+         */
+        extern int maxBuildLevel;
     }
 
 
