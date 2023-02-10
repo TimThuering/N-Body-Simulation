a6b9575d9dfa6d03e1a1801ebecabb985bd495f4
TimThuering <56958925+TimThuering@users.noreply.github.com>
2023-02-10 11:24:35 +0100

    Update BarnesHutTest.cpp

--- tests/BarnesHutTest.cpp
+++ tests/BarnesHutTest.cpp
@@ -1,11 +1,12 @@
-#include <gtest/gtest.h>
-#include "BarnesHutAlgorithm.hpp"
-#include "SimulationData.hpp"
-#include "sycl.hpp"
+ #include <gtest/gtest.h>
+//#include "BarnesHutAlgorithm.hpp"
+//#include "SimulationData.hpp"
+#include <sycl.hpp>
+using namespace sycl;
 
 
 
-
+/*
 TEST(TestTreeCreation, AABB_creation) {
     std::string temp = " ";
     BarnesHutAlgorithm algorithm(1.0/24, 30, 1, temp, 3);
@@ -235,3 +236,269 @@ TEST(TestTreeCreation, buildOctreeTest) {
     EXPECT_EQ(algorithm.bodyOfNode[7], 1); // second body in lower_SW
     EXPECT_EQ(algorithm.bodyOfNode[8], 3);
 }
+
+TEST(TestTreeCreation, fenceTest) {
+
+    queue queue;
+
+    int n = 500;
+
+
+    std::vector<int> Test_vec(n + 1, -1);
+    buffer<int> Test = Test_vec;
+
+    std::vector<int> Test_vec2(n + 1, -1);
+    buffer<int> Test2 = Test_vec2;
+
+
+    std::vector<std::size_t> nextFreeID_vec(1, 1);
+    buffer<std::size_t> nextFreeID = nextFreeID_vec;
+//
+    std::vector<int> lock_vec(1, 0);
+    buffer<int> lock = lock_vec;
+
+
+    queue.submit([&](handler &h) {
+
+
+//
+//
+        accessor<std::size_t> NEXT_FREE_ID(nextFreeID, h);
+        accessor<int> LOCK(lock, h);
+        accessor<int> TEST_ACC(Test, h);
+        accessor<int> TEST_ACC2(Test2, h);
+
+
+        h.parallel_for(nd_range<1>(range<1>(n), range<1>(n)), [=](auto &i) {
+//        h.parallel_for(n, [=](auto &i) {
+
+
+
+
+            int index = 0;
+
+
+            bool success = false;
+            int k = 0;
+//
+            while (!success) {
+
+
+                int exp = 0;
+                atomic_ref<int, memory_order::acq_rel, memory_scope::device,
+                        access::address_space::global_space> lockAccessor(LOCK[index]);
+
+                atomic_ref<std::size_t, memory_order::acq_rel, memory_scope::device,
+                        access::address_space::global_space> nextFreeIDAccessor(NEXT_FREE_ID[0]);
+
+
+//
+
+                if (lockAccessor.compare_exchange_strong(exp, 1, memory_order::acq_rel,
+                                                         memory_scope::device)) {
+                    std::size_t nextFreeID;
+                    nextFreeID = nextFreeIDAccessor.fetch_add(1);
+//                    TEST_ACC[nextFreeID] = 50000;
+//
+//                    i.barrier();
+//                                       sycl::atomic_fence(memory_order::acq_rel, memory_scope::device);
+                    lockAccessor.fetch_sub(1, memory_order::acq_rel, memory_scope::device);
+
+                    success = true;
+
+
+                }
+
+                k++;
+
+            }
+
+
+        });
+    }).wait();
+
+    host_accessor acc(Test2);
+    host_accessor acc2(nextFreeID);
+
+//    for (int i = 0; i < n + 1; ++i) {
+//        std::cout << acc[i] << std::endl;
+//    }
+    std::cout << acc2[0];
+
+}
+ */
+
+TEST(TestTreeCreation, testMemFence) {
+
+    queue queue;
+
+    int size = 64000;
+
+    std::vector<long> testVec(size + 1, -1);
+
+    std::vector<long> resVec(size + 1);
+
+    std::vector<int> lockVec(1, 0);
+
+    std::vector<int> counterVec(1, 1);
+
+    buffer<long> testBuff = testVec;
+
+    buffer<long> resBuff = resVec;
+
+    buffer<int> lockBuff = lockVec;
+
+    buffer<int> counterBuff = counterVec;
+
+
+    queue.submit([&](handler &h) {
+
+        accessor data(testBuff, h);
+
+        accessor lock(lockBuff, h);
+
+        accessor res(resBuff, h);
+
+        accessor counter(counterBuff, h);
+
+
+
+
+
+
+
+         h.parallel_for(size, [=](auto &i) {
+
+//        h.parallel_for(nd_range<1>(range<1>(size), range<1>(size)), [=](auto &nd_item) {
+
+
+            int exp = 0;
+
+            atomic_ref<int, memory_order::acq_rel, memory_scope::device,
+
+                    access::address_space::global_space> atomicNodeIsLockedAccessor(
+
+                    lock[0]);
+
+
+            atomic_ref<int, memory_order::acq_rel, memory_scope::device,
+
+                    access::address_space::global_space> atomicCounter(
+
+                    counter[0]);
+
+
+            bool operationDone = false;
+
+
+            while (!operationDone) {
+
+
+
+
+                if (atomicNodeIsLockedAccessor.compare_exchange_strong(exp, 1, memory_order::acq_rel,
+                                                                       memory_scope::device)) {
+
+                    int index = atomicCounter.fetch_add(1, memory_order::acq_rel, memory_scope::device);
+
+//                    data[index] = nd_item.get_global_id();
+                    data[index] = i;
+
+//                    nd_item.mem_fence(access::fence_space::global_and_local);
+                    atomic_fence(memory_order::acq_rel,memory_scope::device);
+
+//                    nd_item.mem_fence(access::fence_space::global_and_local);
+
+
+                    operationDone = true;
+
+                    atomicNodeIsLockedAccessor.fetch_sub(1, memory_order::acq_rel, memory_scope::device);
+//                    res[nd_item.get_global_id()] = data[index - 1];
+                                        res[i] = data[index - 1];
+
+
+
+
+                }
+
+            }
+
+
+        });
+
+    }).wait();
+
+
+    host_accessor test(resBuff);
+
+    for (int i = 0; i <= size; ++i) {
+
+        if (test[i] == -1) {
+            std::cout << i << " :  " << test[i] << std::endl;
+
+        }
+
+
+    }
+
+}
+
+
+ TEST(TestTreeCreation, testrsqrt) {
+
+     queue queue;
+
+     int size = 64000;
+
+     std::vector<double> testVec(size + 1, -1);
+
+
+
+     buffer<double> testBuff = testVec;
+
+
+
+     queue.submit([&](handler &h) {
+
+         accessor data(testBuff, h);
+
+
+
+
+
+
+
+
+
+         h.single_task( [=]() {
+             double x1 = 500000000.123546;
+             double x2 = 500000000.123546;
+             for (int i = 0; i < 10; ++i) {
+
+                 x1 = 1 / sycl::sqrt(x1);
+                 x2 = sycl::rsqrt(x2);
+
+
+             }
+
+             data[0] = x1;
+             data[1] = x2;
+
+
+
+
+
+         });
+
+     }).wait();
+
+
+     host_accessor test(testBuff);
+
+     std::cout << test[0] << std::endl;
+     std::cout << test[1] << std::endl;
+
+
+
+ }
+
